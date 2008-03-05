cfd955604648aaab71bd9bf6c9c2879e931ca418
Andreas Zeidler <zeidler@jarn.com>
2008-03-05 12:00:01 +0000

    simplified test setup
    git-svn-id: https://svn.plone.org/svn/collective/buildout/collective.recipe.solrinstance/trunk@60001 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/recipe/solrinstance/tests/test_docs.py
+++ collective/recipe/solrinstance/tests/test_docs.py
@@ -1,48 +1,30 @@
-# -*- coding: utf-8 -*-
 """
 Generic Test case for 'collective.recipe.solrinstance' doctest
 """
-__docformat__ = 'restructuredtext'
 
 import unittest
 import doctest
 import sys
 import os
 
-from zope.testing import doctest
+from zope.testing import doctestunit
 import zc.buildout.testing
 
-current_dir = os.path.dirname(__file__)
+flags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE |
+         doctest.REPORT_ONLY_FIRST_FAILURE)
 
 def setUp(test):
     zc.buildout.testing.buildoutSetUp(test)
     zc.buildout.testing.install_develop('collective.recipe.solrinstance', test)
 
-def doc_suite(test_dir, setUp=None, tearDown=None, globs=None):
-    """Returns a test suite, based on doctests found in /doctest."""
-    suite = []
-    if globs is None:
-        globs = globals()
-
-    globs['test_dir'] = current_dir
-
-    flags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE |
-             doctest.REPORT_ONLY_FIRST_FAILURE)
-
-    package_dir = os.path.split(test_dir)[0]
-    if package_dir not in sys.path:
-        sys.path.append(package_dir)
-
-    suite.append(doctest.DocFileSuite('../README.txt', optionflags=flags,
-                                  globs=globs, setUp=setUp,
-                                  tearDown=tearDown,
-                                  module_relative=True))
-
-    return unittest.TestSuite(suite)
-
 def test_suite():
-    """returns the test suite"""
-    return doc_suite(current_dir, setUp=setUp)
+    """ returns the test suite """
+    return unittest.TestSuite([
+        doctestunit.DocFileSuite(
+           'README.txt', package='collective.recipe.solrinstance',
+           optionflags=flags, setUp=setUp)
+    ])
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
+
