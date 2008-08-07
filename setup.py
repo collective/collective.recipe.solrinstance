43600291df4a9a678e3fe2c2bfc3a5e1b36e26fc
Jan Murre <jan.murre@xs4all.nl>
2008-08-07 18:17:05 +0000

    set version nr. to 0.2
    git-svn-id: https://svn.plone.org/svn/collective/buildout/collective.recipe.solrinstance/tags/0.2@69640 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of collective.recipe.solrinstance
 import os
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
