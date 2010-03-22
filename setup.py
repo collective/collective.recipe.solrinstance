f5462f6ac70d1125b469f93fa191caec5f7e26c6
Hanno Schlichting <hanno@jarn.com>
2010-03-22 13:17:46 +0000

    Simplify setup.py
    git-svn-id: https://svn.plone.org/svn/collective/buildout/collective.recipe.solrinstance/trunk@113688 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
-This module contains the tool of collective.recipe.solrinstance
-"""
+
 import os
 from setuptools import setup, find_packages
 
 version = '1.0'
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-entry_point = 'collective.recipe.solrinstance:Recipe'
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
 
-entry_points = {"zc.buildout": ["default = %s" % entry_point]}
 
 setup(name='collective.recipe.solrinstance',
       version=version,
@@ -22,14 +18,13 @@ setup(name='collective.recipe.solrinstance',
         + '\n' +
         read('CHANGES.txt')
         + '\n' +
-        'Contributors\n' 
+        'Contributors\n'
         '***********************\n'
         + '\n' +
         read('CONTRIBUTORS.txt')
         + '\n' +
         'Download\n'
-        '***********************\n'
-        ),
+        '***********************\n'),
       classifiers=[
         'Framework :: Buildout',
         'Intended Audience :: Developers',
@@ -45,13 +40,14 @@ setup(name='collective.recipe.solrinstance',
       namespace_packages=['collective', 'collective.recipe'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
-                        'iw.recipe.template',
-                        'zc.buildout'
-                        ],
-      tests_require=['zope.testing',
-                     'iw.recipe.template',
-                    ],
+      install_requires=[
+        'setuptools',
+        'iw.recipe.template',
+        'zc.buildout'],
+      tests_require=[
+        'zope.testing',
+        'iw.recipe.template'],
       test_suite = 'collective.recipe.solrinstance.tests.test_suite',
-      entry_points=entry_points,
+      entry_points = {
+        "zc.buildout": ["default = collective.recipe.solrinstance:Recipe"]},
       )
