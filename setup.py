73d196bfdc88a5b92ca205a65ac02ceb78d8c331
Josip Delic <jd@lugensa.com>
2011-04-12 10:09:20 +0000

    new:release with resturedText fixed
    git-svn-id: https://svn.plone.org/svn/collective/buildout/collective.recipe.solrinstance/trunk@237406 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '2.1dev'
+version = '2.1'
 
 def read(name):
     return open(os.path.join(os.path.dirname(__file__), name)).read()
