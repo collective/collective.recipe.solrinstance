db68782f2ed83b204bed2ffac4242b1b352c70cf
Hanno Schlichting <hanno@jarn.com>
2011-06-23 12:00:14 +0000

    Forgot to update the version number in setup.py
    git-svn-id: https://svn.plone.org/svn/collective/buildout/collective.recipe.solrinstance/trunk@241351 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '3.1.1'
+version = '3.2'
 
 def read(name):
     return open(os.path.join(os.path.dirname(__file__), name)).read()
