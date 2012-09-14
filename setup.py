20b104db7b7e95e45f453f536510e2f29e1ca41d
Clayton Parker <clayton@sixfeetup.com>
2012-09-14 10:53:30 -0400

    PEP8

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ from setuptools import setup, find_packages
 
 version = '3.9.dev0'
 
+
 def read(name):
     return open(os.path.join(os.path.dirname(__file__), name)).read()
 
 
-setup(name='collective.recipe.solrinstance',
-      version=version,
-      description="zc.buildout to configure a solr instance",
-      long_description= (
+setup(
+    name='collective.recipe.solrinstance',
+    version=version,
+    description="zc.buildout to configure a solr instance",
+    long_description=(
         read('README.txt')
         + '\n' +
         read('CHANGES.txt')
@@ -24,34 +26,37 @@ setup(name='collective.recipe.solrinstance',
         + '\n' +
         'Download\n'
         '***********************\n'),
-      classifiers=[
+    classifiers=[
         'Framework :: Buildout',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Zope Public License',
         'Topic :: Software Development :: Build Tools',
-        ],
-      keywords='',
-      author='Kai Lautaportti',
-      author_email='kai.lautaportti@hexagonit.fi',
-      url='http://pypi.python.org/pypi/collective.recipe.solrinstance',
-      license='ZPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective', 'collective.recipe'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+    ],
+    keywords='',
+    author='Kai Lautaportti',
+    author_email='kai.lautaportti@hexagonit.fi',
+    url='http://pypi.python.org/pypi/collective.recipe.solrinstance',
+    license='ZPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.recipe'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         'setuptools',
         'setuptools-git',
         'iw.recipe.template',
-        'zc.buildout'],
-      tests_require=[
+        'zc.buildout',
+    ],
+    tests_require=[
         'zope.exceptions',
         'zope.interface',
-        'zope.testing'],
-      test_suite = 'collective.recipe.solrinstance.tests.test_suite',
-      entry_points = {
-        "zc.buildout": ["default = collective.recipe.solrinstance:SolrSingleRecipe",
-                        "mc = collective.recipe.solrinstance:MultiCoreRecipe",
-                        ]
-        },
-      )
+        'zope.testing',
+    ],
+    test_suite='collective.recipe.solrinstance.tests.test_suite',
+    entry_points={
+        "zc.buildout": [
+            "default = collective.recipe.solrinstance:SolrSingleRecipe",
+            "mc = collective.recipe.solrinstance:MultiCoreRecipe",
+        ]
+    },
+)
